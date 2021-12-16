fe033264da02e0faab746551a853c35a18c2ab14
Jack Yang <1653319412@qq.com>
2021-12-16 11:04:31 +0800

    dp arrange&combination

---
+++ dynamic-programming/0322-coin-change.py
@@ -0,0 +1,15 @@
+#https://leetcode-cn.com/problems/coin-change/submissions/
+#https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0322.%E9%9B%B6%E9%92%B1%E5%85%91%E6%8D%A2.md
+# 完全背包 组合
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # 初始化
+        dp = [amount + 1]*(amount + 1)
+        dp[0] = 0
+        # 遍历物品
+        for coin in coins:
+            # 遍历背包
+            for j in range(coin, amount + 1):
+                dp[j] = min(dp[j], dp[j - coin] + 1)
+        return dp[amount] if dp[amount] < amount + 1 else -1
---
+++ dynamic-programming/5-longest-palindromic-substring.py
@@ -0,0 +1,25 @@
+#https://leetcode-cn.com/problems/longest-palindromic-substring/
+#https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0005.%E6%9C%80%E9%95%BF%E5%9B%9E%E6%96%87%E5%AD%90%E4%B8%B2.md
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        dp = [[False] * len(s) for _ in range(len(s))]
+        maxlength = 0
+        left = 0
+        right = 0
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i, len(s)):
+                if s[j] == s[i]:
+                    if j - i <= 1 or dp[i+1][j-1]:
+                        dp[i][j] = True
+                if dp[i][j] and j - i + 1 > maxlength:
+                    maxlength = j - i + 1
+                    left = i
+                    right = j
+        return s[left:right + 1]
+
+
+
+
+
