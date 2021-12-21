67395e10a5138ef21f546a5f9e1637369609b4c3
Jack Yang <1653319412@qq.com>
2021-12-21 15:21:54 +0800

    Update 378.kth-smallest-element-in-a-sorted-matrix.py

--- list/378.kth-smallest-element-in-a-sorted-matrix.py
+++ list/378.kth-smallest-element-in-a-sorted-matrix.py
@@ -1,69 +1,37 @@
 # https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/
 
+"""
+找第K个最小元素
+就是当在两个值之间找第k个元素
+
+"""
+
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n = len(matrix)
-        start, end = matrix[0][0], matrix[-1][-1]
-
-        while start < end:
-            mid = (start + end) // 2
-    
-
-    def countMid(self, mid, matrix, k):
+    def kthSmallest(self, matrix, k):
 
         n = len(matrix)
-        row, col = n-1, 0
-        count = 0
 
-        while row >= 0 and col < n:
-            if matrix[row][col] <= mid:
-                count += row + 1
-                col += 1
+        def check(mid):
+            i, j = n-1, 0
+            num = 0
+            while i >= 0 and j < n:
+                if matrix[i][j] <= mid:
+                    num += j + 1
+                    j += 1
+                else:
+                    i -= 1
+            return num >= k
+
+        left, right = matrix[0][0], matrix[-1][-1]
+
+        while left < right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid
             else:
-                col -= 1
-        
-        if count >= k:
-            return True
+                left = mid + 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return left
 
 
 
