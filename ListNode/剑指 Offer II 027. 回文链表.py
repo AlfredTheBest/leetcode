f3b7f12c11550e8190deccca0eecf0b6bc13fb02
AlfredTheBest <1653319412@qq.com>
2021-12-30 00:24:52 +0800

    listnode pat2

---
+++ ListNode/剑指 Offer II 027. 回文链表.py
@@ -0,0 +1,32 @@
+"""
+https://leetcode-cn.com/problems/aMhZSa/
+思路:
+    反转链表比较
+"""
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow, fast, pre = head, head , None
+        while fast and fast.next:
+            fast = fast.next.next
+            temp = slow.next
+            slow.next = pre
+            pre = slow
+            slow = temp
+        if fast:
+            slow = slow.next
+        while slow:
+            if slow.val != pre.val:
+                return False
+
+        return True
+
+
+
+
+
+
+
+
+
+
+
---
+++ ListNode/剑指 Offer II 029. 排序的循环链表.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode-cn.com/problems/4ueAj6/
+思路:
+    在递增区间插入或者 递减且 比最小值小或比最小值大
+"""
+class Solution:
+    def insert(self, head: 'Node', insertVal: int) -> 'Node':
+        node = Node(insertVal)
+        if not head:
+            node.next = node
+            return node
+        if head == head.next:
+            head.next = node
+            node.next = head
+            return head
+        cur=head
+        while cur.next != head:
+            if cur.val <= insertVal <= cur.next.val:
+                break
+            if cur.val > cur.next.val and (cur.val < insertVal or insertVal < cur.next.val):
+                break
+            cur = cur.next
+        node.next = cur.next
+        cur.next = node
+        return head
