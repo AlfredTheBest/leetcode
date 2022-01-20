1aae2e7e69ef086d368073479e578bd17f5e737e
AlfredTheBest <1653319412@qq.com>
2022-01-20 23:38:33 +0800

    dp part 2

---
+++ dp/剑指 Offer II 092. 翻转字符.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode-cn.com/problems/cyJERH/
+思路:
+    dp
+"""
+
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        dp = [[0,1] if s[0] == '0' else [1,0]]
+
+        for i in range(1, len(s)):
+            dp.append([dp[-1][0] + 1, min(dp[-1][1], dp[-1][0])] if s[i] == '1' else [dp[-1][0], dp[-1][1]+1])
+
+
+
+
+
+
---
+++ dp/剑指 Offer II 093. 最长斐波那契数列.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode-cn.com/problems/Q91FMA/
+思路:
+    dp
+"""
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        n = len(arr)
+        record = {}
+        # dp[i][j] 表示以 arr[i], arr[j] 为结尾的 j 状态下的最长斐波那契序列长度
+        dp = [[2]*n for _ in range(n)]
+        ans = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                before = arr[j] - arr[i]
+                if before in record:
+                    dp[i][j] = max(dp[i][j], dp[record[before]][i] + 1)
+                    ans = max(ans, dp[i][j])
+
+            record[arr[i]] = i
+
+        return ans if ans > 2 else 0
+
+
+
+
+
+
+
+
+
